3e42395b24aee396e4967f830dd131439daa07f5
jk <vargsmane@gmx.de>
2016-12-14 12:25:23 +0100

    + added actions for comments

---
+++ src/actions/commentsActions.js
@@ -0,0 +1,35 @@
+import * as types from '../constants/actionTypes';
+import { Parse } from 'parse';
+
+export function loadComments() {
+  return function (dispatch) {
+    const Comment = Parse.Object.extend("Comment");
+    const query = new Parse.Query(Comment);
+
+    query.find({
+      success: function (results) {
+        for (let i = 0; i < results.length; i++) {
+          const result = results[i];
+          dispatch(addComment(
+            result.id,
+            result.get("text"),
+            result.get("parentID"),
+          ));
+        }
+      },
+      error: function (error) {
+        console.log(error);
+      }
+    });
+  };
+}
+
+export function addComment(id, text, parentID) {
+  console.log(id, text, parentID);
+  return {
+    type: types.ADD_COMMENT,
+    text: text,
+    parentID: parentID,
+    id: id
+  };
+}
