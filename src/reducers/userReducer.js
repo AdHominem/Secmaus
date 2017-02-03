bf7f47b06c50cf2c6c892474fcf5858558ae3cc2
Leon Rische <leon.rische@student.uni-siegen.de>
2017-02-03 03:35:05 +0100

    Rename userReducer

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,6 +1,6 @@
 import { SET_IS_ADMIN } from '../constants/actionTypes';
 
-export default function commentsReducer(state = {isAdmin: false}, action) {
+export default function userReducer(state = {isAdmin: false}, action) {
   switch (action.type) {
     case SET_IS_ADMIN:
       return {isAdmin: true};
