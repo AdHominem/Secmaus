0813f9777e842002ca07a26561f98a76adbcd2a2
Leon Rische <leon.rische@student.uni-siegen.de>
2017-02-15 15:47:23 +0100

    Refactoring

--- src/components/Question/SingleChoiceForm.js
+++ src/components/Question/SingleChoiceForm.js
@@ -1,28 +1,20 @@
 import React, { PropTypes, Component } from 'react';
-import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
-import * as actions from '../../actions/pollsActions';
 
-const SingleChoiceForm = props => {
-  const { question, value, onChange } = props;
-
-  return (
-    <div className="question">
-      <h1>{question.text}</h1>
-      <form>
-      { question.choices.map((choice, i) => (
-          <label key={i}>
-            <input
-              type="radio" name={choice}
-              checked={value === i}
-              onChange={onChange(i)}
-            />
-            {choice}
-          </label>
-        )) }
-      </form>
-    </div>
-  );
-}
+const SingleChoiceForm = ({ question, value, onChange }) =>
+  <div className="question">
+    <h1>{question.text}</h1>
+    <form>
+    { question.choices.map((choice, i) => (
+        <label key={i}>
+          <input
+            type="radio" name={choice}
+            checked={value === i}
+            onChange={onChange(i)}
+          />
+          {choice}
+        </label>
+      )) }
+    </form>
+  </div>;
 
 export default SingleChoiceForm;
