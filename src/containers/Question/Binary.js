7054fc128cdef42f26e3daecd8d9e00684085d7e
jk <vargsmane@gmx.de>
2017-02-15 21:00:24 +0100

    Refactoring

--- src/containers/Question/Binary.js
+++ src/containers/Question/Binary.js
@@ -1,13 +1,19 @@
-import React, { PropTypes, Component } from 'react';
+import React, { PropTypes } from 'react';
 import { Pie } from 'react-chartjs-2';
 import { map, nth, pipe, equals } from 'ramda';
+
 import { binaryColors } from '../../constants/colors';
 import { count } from '../../utils/count';
 
-const BinaryQuestion = ({ question, question: { choices, answers } }) => {
-  const stats = ["Ja", "Nein"].map((choice, i) => (
+const binaryQuestionRequiredProps = {
+  question: PropTypes.object.isRequired
+};
+
+function BinaryQuestion({ question: { text, answers } }) {
+
+  const stats = ["Ja", "Nein"].map((choice, i) =>
     [choice, count(pipe(nth(1), equals(i)), answers)]
-  ));
+  );
 
   const data = {
     labels: map(nth(0), stats),
@@ -22,10 +28,12 @@ const BinaryQuestion = ({ question, question: { choices, answers } }) => {
 
   return (
     <div className="question">
-      <h1>{question.text}</h1>
+      <h1>{text}</h1>
       <Pie data={data} />
     </div>
   );
 }
+
+BinaryQuestion.propTypes = binaryQuestionRequiredProps;
 
 export default BinaryQuestion;
