b599eaa1c26e743fa77d5b47d26193b9adfb0e61
jk <vargsmane@gmx.de>
2017-02-22 11:14:11 +0100

    Linting

--- src/containers/Poll.js
+++ src/containers/Poll.js
@@ -10,7 +10,7 @@ import SingleChoiceQuestion from "./Question/SingleChoice";
 import BinaryForm from "../presentational/BinaryForm";
 import LikertForm from "./Question/LikertForm";
 import SingleChoiceForm from "./Question/SingleChoiceForm";
-import { sortBy, pipe, sort, prop, propEq, map, update, any, clone } from "ramda";
+import { sortBy, pipe, prop, propEq, update, any } from "ramda";
 import FontAwesome from 'react-fontawesome';
 import Modal from 'react-modal';
 import PollForm from './PollForm';
@@ -20,7 +20,10 @@ class Poll extends Component {
 
   static propTypes = {
     pollsActions: PropTypes.object.isRequired,
-    poll: PropTypes.object.isRequired
+    poll: PropTypes.object.isRequired,
+    isAdmin: PropTypes.bool.isRequired,
+    questionsActions: PropTypes.object.isRequired,
+    questions: PropTypes.object.isRequired
   };
 
   constructor(props) {
@@ -54,18 +57,18 @@ class Poll extends Component {
       poll: {
         id, text, closed, measureId
       },
-      pollsActions: { closePoll, editPoll, deletePoll, answerPoll },
+      pollsActions: { closePoll, deletePoll },
       questionsActions: { answerQuestion },
       questions
     } = this.props;
 
     const { answers } = this.state;
 
-    const selectAnswer = index => value => event => {
+    const selectAnswer = index => value => () => {
       this.setState({ answers: update(index, value, answers) });
     };
 
-    const submitAnswers = event => {
+    const submitAnswers = () => {
       questions.forEach((question, i) => {
         answerQuestion(question.id, answers[i]);
       });
@@ -88,27 +91,27 @@ class Poll extends Component {
       event.preventDefault();
     };
 
-    let alreadyAnswered = questions.length > 0 && any(answer => (answer[0] === Parse.User.current().id), questions[0].answers);
+    let alreadyAnswered = questions.length > 0 && any(answer => answer[0] === Parse.User.current().id, questions[0].answers);
 
 
     const selectQuestionForm = (question, i) =>
       <div key={i}>
         { alreadyAnswered = closed || alreadyAnswered }
-        {(question.questionType === "binary") ?
+        {question.questionType === "binary" ?
           alreadyAnswered ? <BinaryQuestion question={question}/>
             : <BinaryForm
                 question={question}
                 value={answers[i]}
                 onChange={selectAnswer(i)}
               />
-        : (question.questionType === "likert") ?
+        : question.questionType === "likert" ?
           alreadyAnswered ? <LikertQuestion question={question}/>
             : <LikertForm
             question={question}
             value={answers[i]}
             onChange={selectAnswer(i)}
           />
-        : (question.questionType === "single choice") ?
+        : question.questionType === "single choice" ?
           alreadyAnswered ? <SingleChoiceQuestion question={question}/>
             : <SingleChoiceForm
             question={question}
@@ -122,7 +125,7 @@ class Poll extends Component {
       <div className="flex-box poll">
         <h1 className="flex-title" dangerouslySetInnerHTML={{__html: text}}/>
         <div className="flex-content">
-          <a onClick={ toggleClose }>{ closed ? <FontAwesome name="lock" size="2x"/> : <FontAwesome name="unlock-alt" size="2x"/> }</a>
+          <a onClick={toggleClose}>{ closed ? <FontAwesome name="lock" size="2x"/> : <FontAwesome name="unlock-alt" size="2x"/> }</a>
           &nbsp;&nbsp;
 
           <Modal
@@ -131,18 +134,18 @@ class Poll extends Component {
             contentLabel="Umfrage editieren"
           >
             <PollForm
-              pollsActions={ pollsActions }
-              measureId={ measureId }
-              poll={ poll }
-              questions={ questions }
-              text = { text }
+              pollsActions={pollsActions}
+              measureId={measureId}
+              poll={poll}
+              questions={questions}
+              text = {text}
             />
           </Modal>
 
-          <a onClick={ onClick }><FontAwesome name="edit" size="2x"/></a>
+          <a onClick={onClick}><FontAwesome name="edit" size="2x"/></a>
 
           &nbsp;&nbsp;
-          <a onClick={ handleDeletePoll }><FontAwesome name="trash" size="2x"/></a>
+          { isAdmin && <a onClick={handleDeletePoll}><FontAwesome name="trash" size="2x"/></a> }
           &nbsp;&nbsp;
           { pipe(sortBy(prop('index')))(questions).map(selectQuestionForm) }
           { !alreadyAnswered && <button onClick={submitAnswers} >Antworten</button> }
