eb5fd5a6a5cf3c3b8fdf5140f0b858fbfcbd8e0b
jk <vargsmane@gmx.de>
2017-02-26 20:22:11 +0100

    Refactoring

--- src/presentational/SingleChoiceForm.js
+++ src/presentational/SingleChoiceForm.js
@@ -1,20 +1,31 @@
-import React, { PropTypes, Component } from 'react';
+import React, { PropTypes } from 'react';
 
-const SingleChoiceForm = ({ question, value, onChange }) =>
-  <div className="question">
-    <h1>{question.text}</h1>
-    <form>
-    { question.choices.map((choice, i) => (
-        <label key={i}>
-          <input
-            type="radio" name={choice}
-            checked={value === i}
-            onChange={onChange(i)}
-          />
-          {choice}
-        </label>
-      )) }
-    </form>
-  </div>;
+const singleChoiceQuestionFormRequiredProps = {
+  question: PropTypes.object.isRequired,
+  onChange: PropTypes.func.isRequired,
+  value: PropTypes.number.isRequired
+};
+
+function SingleChoiceForm({ question, value, onChange }) {
+  return (
+    <div className="question">
+      <h1>{question.text}</h1>
+      <form>
+      { question.choices.map((choice, i) => (
+          <label key={i}>
+            <input
+              type="radio" name={choice}
+              checked={value === i}
+              onChange={onChange(i)}
+            />
+            {choice}
+          </label>
+        )) }
+      </form>
+    </div>
+  );
+}
+
+SingleChoiceForm.propTypes = singleChoiceQuestionFormRequiredProps;
 
 export default SingleChoiceForm;
