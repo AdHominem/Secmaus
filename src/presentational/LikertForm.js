7813a1f28686219216a1387f75723589d1cee6a9
jk <vargsmane@gmx.de>
2017-02-26 20:21:56 +0100

    Refactoring

--- src/presentational/LikertForm.js
+++ src/presentational/LikertForm.js
@@ -1,25 +1,37 @@
-import React, { PropTypes, Component } from 'react';
+import React, { PropTypes } from 'react';
+
 import { likertLabels } from '../constants/labels.js';
 
-const LikertForm = ({ question, value, onChange }) =>
-  <div className="question">
-      <h1>{question.text}</h1>
-      <form>
-        <ul className="likert">
-        { likertLabels.map((label, i) => (
-            <li key={i}>
-              <label>
-                <input
-                  type="radio" name={label}
-                  checked={value === i}
-                  onChange={onChange(i)}
-                />
-                {label}
-              </label>
-            </li>
-          )) }
-        </ul>
-      </form>
-    </div>
+const likertFormRequiredProps = {
+  question: PropTypes.object,
+  value: PropTypes.number,
+  onChange: PropTypes.func
+};
+
+function LikertForm({ question, value, onChange }) {
+  return (
+    <div className="question">
+        <h1>{question.text}</h1>
+        <form>
+          <ul className="likert">
+          { likertLabels.map((label, i) => (
+              <li key={i}>
+                <label>
+                  <input
+                    type="radio" name={label}
+                    checked={value === i}
+                    onChange={onChange(i)}
+                  />
+                  {label}
+                </label>
+              </li>
+            )) }
+          </ul>
+        </form>
+      </div>
+    );
+}
+
+LikertForm.propTypes = likertFormRequiredProps;
 
 export default LikertForm;
