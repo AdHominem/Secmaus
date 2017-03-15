8e3fd8bf6033477eec119b7aa6083efb81940a9c
jk <vargsmane@gmx.de>
2017-03-15 13:52:46 +0100

    Disabled Submit button for empty text fields

--- src/presentational/ButtonRow.js
+++ src/presentational/ButtonRow.js
@@ -3,13 +3,14 @@ import React, { PropTypes } from 'react';
 const buttonRowRequiredProps = {
   onClose: PropTypes.func.isRequired,
   onSubmit: PropTypes.func.isRequired,
+  disableSubmit: PropTypes.bool
 };
 
-function ButtonRow({ onClose, onSubmit }) {
+function ButtonRow({ onClose, onSubmit, disableSubmit}) {
   return (
     <div className="button-row">
       <button className="button-danger" onClick={onClose}>Abbrechen</button>
-      <button className="button-success" onClick={onSubmit}>OK</button>
+      <button className="button-success" onClick={onSubmit} disabled={disableSubmit}>OK</button>
     </div>
   );
 }
