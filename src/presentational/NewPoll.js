435c1291ecfcc88f8b274c57145bbd3329fcf2f7
Leon Rische <leon.rische@student.uni-siegen.de>
2017-03-22 13:04:00 +0100

    Add default Poll template

--- src/presentational/NewPoll.js
+++ src/presentational/NewPoll.js
@@ -8,11 +8,37 @@ const newPollRequiredProps = {
 
 function NewPoll(props) {
   const { params: { measureId } } = props;
+  const questions = [
+    {
+      text: "Wie stark ist der Einfluss der Sicherheitsmaßnahme auf Ihren täglichen Arbeitsablauf",
+      questionType: "likert",
+      choices: [],
+    },
+    {
+      text: "Konnten Sie die Notwendigkeit der Sicherheitsmaßnahme nachvollziehen?",
+      questionType: "binary",
+      choices: [],
+    },
+    {
+      text: "Welche Verbesserungsvorschläge haben Sie für uns? Gibt es Sicherheitslücken, die wir bisher nicht beachtet haben?",
+      questionType: "freeform",
+      choices: [],
+    },
+    {
+      text: "Wie wahrscheinlich ist es dass Sie einer erneuten Einführung dieser Sicherheitsmaßnahme zustimmen?",
+      questionType: "likert",
+      choices: [],
+    }
+  ]
 
   return (
     <div className="new-poll">
       <h2>Umfrage hinzufügen</h2>
-      <PollForm measureId={ measureId } />
+      <PollForm
+        text="Nutzerbefragung zur Sicherheitsmaßnahme"
+        questions={ questions }
+        measureId={ measureId }
+      />
     </div>
   );
 }
